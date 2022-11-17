30f66baaab280993d923b3dd20d750de436c47b2
aspanyndr <aspanyundar20@gmail.com>
2022-11-18 02:15:32 +0700

    commit integration

---
+++ belajar-integration/demo-api/src/main/java/com/domain/demoapi/FilterMoveRoute.java
@@ -0,0 +1,14 @@
+package com.domain.demoapi;
+
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FilterMoveRoute  extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+    }
+}
